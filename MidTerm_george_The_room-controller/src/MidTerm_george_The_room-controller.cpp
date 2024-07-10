0a67d013360652cc3c4bd91a8400dcbb9444e866
jonhtanan richrs <jisr1012@gmail.com>
2024-07-10 15:28:25 -0600

    working temp wemo controller

--- MidTerm_george_The_room-controller/src/MidTerm_george_The_room-controller.cpp
+++ MidTerm_george_The_room-controller/src/MidTerm_george_The_room-controller.cpp
@@ -23,18 +23,24 @@ Adafruit_BME280 bme ;
 bool status;
 float tempC;
 float pressPA;
-float humidRH;
+float humidity;
 float inHg;
 float tempF;
 int currentTime;
 int last150millisSec;
 
+int last1000millisSec;
+
 const int hexAddress = 0x76;
+int last5Seconds;
 
 const int OLED_RESET=-1;
 Adafruit_SSD1306 display(OLED_RESET);
 
-const int LEDPIN = 5;
+const int LEDPIN = D3;
+
+const int MYWEMO=3;
+const int MYWEMO2=0;
 
 // Run the application and system concurrently in separate threads
 //SYSTEM_THREAD(ENABLED);
@@ -49,6 +55,24 @@ void setup() {
     Serial.printf("BME280 at address 0x%02X failed to start", hexAddress);
   }
 
+  WiFi.on();
+  WiFi.clearCredentials();
+  WiFi.setCredentials("IoTNetwork");
+
+  WiFi.connect();
+  while(WiFi.connecting()) {
+    currentTime = millis();
+    if((currentTime-last5Seconds)>5000) {
+        last5Seconds = millis();
+        Serial.printf("connecting");
+    }
+  }
+
+  if((currentTime-last5Seconds)>5000) {
+        last5Seconds = millis();
+  Serial.printf("\n\n");
+  }
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
   display.display();
@@ -58,15 +82,23 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.printf("tempF %0.2f\n,humidRH %0.2f\n",tempF,humidRH);
+  display.printf("tempF %0.2f\n,humidity %0.2f\n",tempF,humidity);
   display.display();
   display.clearDisplay();
 
-  pinMode(LEDPIN, INPUT_PULLDOWN);
+  pinMode(LEDPIN, OUTPUT);
+
+
+
 }
 
 
 void loop() {
+
+digitalWrite(LEDPIN, HIGH);
+
+
+
   currentTime = millis ();
   if((currentTime-last150millisSec)>150) {
     last150millisSec = millis();
@@ -74,16 +106,39 @@ void loop() {
     tempF = 1.8 * tempC + 32;
   pressPA = bme.readPressure();
     inHg= 0.000295 * pressPA + 4.77;
-  humidRH = bme.readHumidity();
-    Serial.printf("tempF %0.2f\n, inHg %0.2f\n,humidRH %0.2f\n",tempF ,inHg, humidRH);
+  humidity = bme.readHumidity();
+    Serial.printf("---\ntempF %0.2f\n---\ninHg %0.2f\n---\nhumidity %0.2f\n",tempF ,inHg, humidity);
   }
+
+if((currentTime-last1000millisSec)>1000) {
+    last1000millisSec = millis();
+  if(tempF > 80 ) {
+    Serial.printf("Turning on Wemo# %i\n",MYWEMO);
+      wemoWrite(MYWEMO,HIGH);
+    }
+  else{
+    Serial.printf("Turning off Wemo# %i\n",MYWEMO);
+      wemoWrite(MYWEMO,LOW);
+    }
+
+    if(tempF < 80 ) {
+    Serial.printf("Turning on Wemo2# %i\n",MYWEMO2);
+      wemoWrite(MYWEMO2,HIGH);
+    }
+  else{
+    Serial.printf("Turning off Wemo2# %i\n",MYWEMO2);
+      wemoWrite(MYWEMO2,LOW);
+    }
+}
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.printf("tempF %0.2f\n,humidRH %0.2f\n",tempF,humidRH);
+  display.printf("---\ntempF %0.2f\n---\nhumidity %0.2f\n---",tempF,humidity);
   display.display();
   display.clearDisplay();
 
 
+
 }
+
